228e15e73f2d6a627086b3a2fb718e8913b2cf0f
Jim <jim.pollaro@gmail.com>
2017-09-18 18:37:34 -0700

    Turned in version

--- main/apps/sessionWords/views.py
+++ main/apps/sessionWords/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    return render(request,'sessionWords/index.html')
+
+def addWord(request):
+    request.session['word'] = request.POST['word']
+    request.session['color'] = request.POST['color']
+    if 'big' in request.POST:
+        request.session['big'] = 'Big'
+    else:
+        request.session['big'] = 'Small'
+    now = datetime.now().strftime('%-I:%M %p %b %-d %Y')
+    data = {'c':request.session['color'],'w':request.session['word'],'b':request.session['big'],'t':now}
+    if 'text' not in request.session:
+        request.session['text'] = [data]
+    else:
+        request.session['text'].append(data)
+    return redirect('/')
+
+def clear(request):
+    request.session.clear();
+    return redirect('/')
